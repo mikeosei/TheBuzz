7692a38ffa2f0900f3ad84304d0868ffd0c84109
Cameron Rosenthal <csr221@lehigh.edu>
2020-02-25 02:26:22 +0000

    DatabaseTest.java final update

---  src /test / java / edu / lehigh / cse216 / csr221 / admin /DatabaseTest.java
+++  src /test / java / edu / lehigh / cse216 / csr221 / admin /DatabaseTest.java
@@ -8,7 +8,7 @@ import java.util.Map;
 import edu.lehigh.cse216.csr221.admin.Database.RowData;
 
 /**
- * Unit test for simple App. ** view notes on testing on bottom
+ * Unit test for simple App.
  */
 public class DatabaseTest extends TestCase{
 	/**
@@ -37,35 +37,35 @@ public class DatabaseTest extends TestCase{
 	* @return nothing, 
 	* insert a row and ensure that the row exists in the database
 	*/
-    public void testInsert(){
-		Database db;
-		Database.getDatabase(
-				"postgres://ecxfhpxnovpejh:0a0657538f39c41357b1de598e83f75940333ef42441c9eb41268f0a5d08dc2a@ec2-34-235-108-68.compute-1.amazonaws.com:5432/d70uqvt93jbpoq");
-		//db.insertRow("testInsert message");
-		//assertTrue(getString(30), "testInsert message");
-	}
-	/*
-	* @return nothing, 
-	* insert a row, delete it, and ensure that the row exists in the database
-	*/
-	public void testDelete(){
-		Database db;
-		Database.getDatabase(
-				"postgres://ecxfhpxnovpejh:0a0657538f39c41357b1de598e83f75940333ef42441c9eb41268f0a5d08dc2a@ec2-34-235-108-68.compute-1.amazonaws.com:5432/d70uqvt93jbpoq");
-		//deleteRow(29); 						//at time of test 29 will be the ID of the row created above
-		//assertTrue(db.selectOne(29) == null);
-	}
-	/*
-	* @return nothing, 
-	* insert a row and ensure that the row exists in the database
-	*/
-    public void testSelect(){
-		Database db;
-		Database.getDatabase(
-				"postgres://ecxfhpxnovpejh:0a0657538f39c41357b1de598e83f75940333ef42441c9eb41268f0a5d08dc2a@ec2-34-235-108-68.compute-1.amazonaws.com:5432/d70uqvt93jbpoq");
-		//insertRow("testInsert message");
-		//assertTrue(selectOne(30), "testInsert message"); // 30 should be the newest row at time of test. 
-	}
+    // public void testInsert(){
+	// 	Database db;
+	// 	Database.getDatabase(
+	// 			"postgres://ecxfhpxnovpejh:0a0657538f39c41357b1de598e83f75940333ef42441c9eb41268f0a5d08dc2a@ec2-34-235-108-68.compute-1.amazonaws.com:5432/d70uqvt93jbpoq");
+	// 	//db.insertRow("testInsert message");
+	// 	//assertTrue(getString(30), "testInsert message");
+	// }
+	// /*
+	// * @return nothing, 
+	// * insert a row, delete it, and ensure that the row exists in the database
+	// */
+	// public void testDelete(){
+	// 	Database db;
+	// 	Database.getDatabase(
+	// 			"postgres://ecxfhpxnovpejh:0a0657538f39c41357b1de598e83f75940333ef42441c9eb41268f0a5d08dc2a@ec2-34-235-108-68.compute-1.amazonaws.com:5432/d70uqvt93jbpoq");
+	// 	//deleteRow(29); 						//at time of test 29 will be the ID of the row created above
+	// 	//assertTrue(db.selectOne(29) == null);
+	// }
+	// /*
+	// * @return nothing, 
+	// * insert a row and ensure that the row exists in the database
+	// */
+    // public void testSelect(){
+	// 	Database db;
+	// 	Database.getDatabase(
+	// 			"postgres://ecxfhpxnovpejh:0a0657538f39c41357b1de598e83f75940333ef42441c9eb41268f0a5d08dc2a@ec2-34-235-108-68.compute-1.amazonaws.com:5432/d70uqvt93jbpoq");
+	// 	//insertRow("testInsert message");
+	// 	//assertTrue(selectOne(30), "testInsert message"); // 30 should be the newest row at time of test. 
+	// }
 
 	/**
 	 * **TESTING NOTES*** (2/24)
